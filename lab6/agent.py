e23ee300914ae5d2f780ccc41fe578d89ec0816b
tclearyUWA <51070356+thomCleary@users.noreply.github.com>
2021-09-26 21:32:11 +0800

    add skeleton for agent and write functions to visualise the game in the terminal

---
+++ lab6/agent.py
@@ -0,0 +1,24 @@
+""" CITS3001 Lab 6 - Thomas Cleary 21704985 """
+
+import random
+
+class C4Agent:
+
+    def move(self, symbol, board, last_move):
+        '''
+        symbol is the character that represents the agents moves in the board.
+        symbol will be consistent throughout the game
+        board is an array of 7 strings each describing a column of the board
+        last_move is the column that the opponent last droped a piece into (or -1 if it is the firts move of the game).
+        This method should return the column the agent would like to drop their token into.
+        '''
+
+        # If we get to make the first move, go for the centre
+        if last_move == -1:
+            return 3
+
+        while True:
+            move = random.randint(0, 6)
+
+            if len(board[move]) < 6:
+                return move
---
+++ lab6/board.py
@@ -0,0 +1,49 @@
+""" board for connect 4 game defined by question """
+
+
+class C4Board():
+
+    def __init__(self):
+        self.num_cols = 7
+        self.num_rows = 6
+
+        self.board = self.generate_board()
+
+        self.pieces = ["X", "O"]
+
+
+    def reset_board(self):
+        self.board = self.generate_board()
+
+
+    def generate_board(self):
+        return ["" for _ in range(self.num_cols)]
+
+
+    def is_full(self):
+        for col in self.board:
+            if len(col) < self.num_rows:
+                return False
+
+        return True
+
+
+    def is_col_full(self, col_num):
+        if len(self.board[col_num]) == self.num_rows:
+            return True
+        return False
+
+
+    def add_piece(self, col_num, piece_type):
+        if self.is_col_full(col_num):
+            return False
+
+        if piece_type not in self.pieces:
+            return False
+
+        self.board[col_num] = self.board[col_num] + piece_type
+        return True
+
+
+    def is_game_over(self):
+        pass
